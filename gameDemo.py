4152cfb364791849768bb3700291e6b0f298238a
Abhishek Kumar <akuma168@asu.edu>
2019-02-26 22:50:42 -0700

    [Task#94] Create frame selection using mouse pointer.

--- gameDemo.py
+++ gameDemo.py
@@ -18,3 +18,34 @@ print("UDP target port:", UDP_PORT)
 
 sock = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
+
+					 class App(object):
+    def __init__(self, video_src):
+        self.cam = cv2.VideoCapture(video_src)
+        ret, self.frame = self.cam.read()
+        cv2.namedWindow('camshift')
+        cv2.setMouseCallback('camshift', self.onmouse)
+
+        self.selection = None
+        self.drag_start = None
+        self.tracking_state = 0
+        self.show_backproj = False
+
+    def onmouse(self, event, x, y, flags, param):
+        x, y = np.int16([x, y])  # BUG
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.drag_start = (x, y)
+            self.tracking_state = 0
+        if self.drag_start:
+            if flags & cv2.EVENT_FLAG_LBUTTON:
+                h, w = self.frame.shape[:2]
+                xo, yo = self.drag_start
+                x0, y0 = np.maximum(0, np.minimum([xo, yo], [x, y]))
+                x1, y1 = np.minimum([w, h], np.maximum([xo, yo], [x, y]))
+                self.selection = None
+                if x1 - x0 > 0 and y1 - y0 > 0:
+                    self.selection = (x0, y0, x1, y1)
+            else:
+                self.drag_start = None
+                if self.selection is not None:
+                    self.tracking_state = 1
