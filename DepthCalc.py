8cb2c64bf7329b544f19cfa7593369d23186c3bd
Abhishek Kumar <akuma168@asu.edu>
2019-03-11 23:07:49 -0700

    [Taga#94] Added the logic for calculating sum of sqaured differences and disparity

--- DepthCalc.py
+++ DepthCalc.py
@@ -7,3 +7,14 @@ dist_betn_lenses = 54.8
 disparities = 64
 block = 15
 units = 0.001
+
+for x in range(block, left.shape[0] - block - 1):
+    for y in range(block + disparities, left.shape[1] - block - 1):
+        differences = numpy.empty([disparities, 1])
+
+        l = left[(x - block):(x + block), (y - block):(y + block)]
+        for d in range(0, disparities):
+            r = right[(x - block):(x + block), (y - d - block):(y - d + block)]
+            differences[d] = numpy.sum((l[:, :] - r[:, :]) ** 2)
+
+        disparity[x, y] = numpy.argmin(differences)
