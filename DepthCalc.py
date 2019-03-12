2fba996c6e5d895594cf295bae58c7d24e855b3b
Abhishek Kumar <akuma168@asu.edu>
2019-03-11 23:08:48 -0700

    [taiga#94] Calculating depth and disparity of the objects.

--- DepthCalc.py
+++ DepthCalc.py
@@ -18,3 +18,6 @@ for x in range(block, left.shape[0] - block - 1):
             differences[d] = numpy.sum((l[:, :] - r[:, :]) ** 2)
 
         disparity[x, y] = numpy.argmin(differences)
+
+depth = np.zeros(shape=left.shape).astype(float)
+depth[disparity > 0] = (focal_length * dist_betn_lenses) / (units * disparity[disparity > 0])
